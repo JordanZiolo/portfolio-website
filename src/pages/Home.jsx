abd452a46d24ea56403a73c376213942aa6daf40
JordanBuchacz <Zonyy699@gmail.com>
2025-11-07 13:43:51 +0100

    update

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import jordanImg from "../assets/jordan.jpg";
+import { Link } from "react-router-dom";
+import "../App.css"
+
+export default function Home() {
+  return (
+    <section className="home">
+      <div className="hero">
+        <div className="text-section">
+          <h1>Jordan Buchacz</h1>
+          <h2>Software Developer</h2>
+        </div>
+        <div className="image-section">
+          <img src={jordanImg} alt="Jordan Buchacz" />
+        </div>
+      </div>
+      <footer className="footer">Jordan Buchacz Portfolio</footer>
+    </section>
+  );
+}
